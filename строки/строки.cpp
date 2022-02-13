19f94c1ab18d706eaeb2f544aa7ae784e85a831a
sasha <sasha@DESKTOP-RI2S6SM>
2022-02-13 18:27:57 +0200

    Добавьте файлы проекта.

---
+++ строки/строки.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+void step(int n, int m) {
+	int s = pow(n, m);
+	cout << s;
+}
+int main()
+{
+	int a = 0, b = 0;
+	cin >> a >> b;
+	step(a, b);
+
+}
+
+
